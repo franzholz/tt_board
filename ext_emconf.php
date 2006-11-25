32da834e519aada98c5699ddbc3776a90d32eeec
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-11-25 08:32:40 +0000

    make double post check a unsigned 32 bit INTEGER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_board/trunk@4144 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_board"
 #
-# Auto generated 21-08-2006 09:46
+# Auto generated 11-11-2006 11:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Freelancer',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.0.11',
-	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"cb41";s:28:"class.tx_ttboard_wizicon.php";s:4:"2c55";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"061f";s:14:"ext_tables.php";s:4:"0352";s:14:"ext_tables.sql";s:4:"9695";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"a4a6";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"c1fc";s:14:"doc/manual.sxw";s:4:"1a6b";s:20:"lib/board_submit.inc";s:4:"3967";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"a278";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:24:"template/board_help.tmpl";s:4:"54b1";s:25:"template/board_notify.txt";s:4:"53f4";s:29:"template/board_template1.tmpl";s:4:"b849";s:29:"template/board_template2.tmpl";s:4:"d01f";s:29:"template/board_template3.tmpl";s:4:"71c3";s:30:"static/css_style/constants.txt";s:4:"872c";s:26:"static/css_style/setup.txt";s:4:"0e16";s:30:"static/old_style/constants.txt";s:4:"77d6";s:26:"static/old_style/setup.txt";s:4:"fdaf";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"12f7";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"848d";}',
+	'version' => '1.1.1',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"5e68";s:28:"class.tx_ttboard_wizicon.php";s:4:"aff0";s:21:"ext_conf_template.txt";s:4:"dbec";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"e4cd";s:14:"ext_tables.php";s:4:"12a8";s:14:"ext_tables.sql";s:4:"9695";s:23:"flexform_ds_pi_list.xml";s:4:"a333";s:23:"flexform_ds_pi_tree.xml";s:4:"1f9b";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"d4e5";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"c1fc";s:14:"doc/manual.sxw";s:4:"15db";s:20:"lib/board_submit.inc";s:4:"0c3d";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"781e";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:23:"res/icons/fe/thread.gif";s:4:"9aac";s:24:"template/board_help.tmpl";s:4:"ac9b";s:25:"template/board_notify.txt";s:4:"53f4";s:29:"template/board_template1.tmpl";s:4:"f508";s:29:"template/board_template2.tmpl";s:4:"f1be";s:29:"template/board_template3.tmpl";s:4:"71c3";s:30:"static/css_style/constants.txt";s:4:"a204";s:26:"static/css_style/setup.txt";s:4:"abc1";s:30:"static/old_style/constants.txt";s:4:"a457";s:26:"static/old_style/setup.txt";s:4:"5bf1";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"6f07";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"86e8";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -45,6 +45,8 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 			'fh_library' => '0.0.11-',
 		),
+	),
+	'suggests' => array(
 	),
 );
 
--- ext_tables.sql
+++ ext_tables.sql
@@ -14,8 +14,26 @@ CREATE TABLE tt_board (
   hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
   parent int(10) unsigned DEFAULT '0' NOT NULL,
   notify_me tinyint(3) unsigned DEFAULT '0' NOT NULL,
-  doublePostCheck int(11) DEFAULT '0' NOT NULL,
+  doublePostCheck int(11) unsigned DEFAULT '0' NOT NULL,
   PRIMARY KEY (uid),
   KEY parent (pid),
   KEY parent_select (pid,parent)
 );
+
+
+#
+# Table structure for table 'tt_board_parent'
+#
+#CREATE TABLE tt_board_parent (
+#  uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+#  pid int(11) unsigned DEFAULT '0' NOT NULL,
+#  deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
+#  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+#  crdate int(11) unsigned DEFAULT '0' NOT NULL,
+#  last_added int(10) unsigned DEFAULT '0' NOT NULL,
+#  notify_me tinyint(3) unsigned DEFAULT '0' NOT NULL,
+#  PRIMARY KEY (uid),
+#  KEY parent (pid),
+#  KEY parent_select (pid,last_added)
+#);
+
