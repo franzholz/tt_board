cb0f654ca66d0cfd4822ad972a11fff1c078bcdf
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-05 13:29:08 +0000

    upload of 1.2.3 into TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_board/trunk@9099 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_board"
 #
-# Auto generated 14-01-2008 08:14
+# Auto generated 05-05-2008 15:27
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'jambage.com',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.2.2',
-	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"d54a";s:28:"class.tx_ttboard_wizicon.php";s:4:"3e44";s:16:"contributors.txt";s:4:"e2e4";s:21:"ext_conf_template.txt";s:4:"dbec";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"d734";s:14:"ext_tables.php";s:4:"12a8";s:14:"ext_tables.sql";s:4:"4a67";s:23:"flexform_ds_pi_list.xml";s:4:"a333";s:23:"flexform_ds_pi_tree.xml";s:4:"2dde";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"b925";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"5939";s:14:"doc/manual.sxw";s:4:"96be";s:20:"lib/board_submit.inc";s:4:"e9e6";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"24db";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:23:"res/icons/fe/thread.gif";s:4:"9aac";s:24:"template/board_help.tmpl";s:4:"ac9b";s:25:"template/board_notify.txt";s:4:"94a4";s:29:"template/board_template1.tmpl";s:4:"102b";s:29:"template/board_template2.tmpl";s:4:"6eee";s:29:"template/board_template3.tmpl";s:4:"7a11";s:30:"static/css_style/constants.txt";s:4:"4a11";s:26:"static/css_style/setup.txt";s:4:"a8fa";s:30:"static/old_style/constants.txt";s:4:"7a40";s:26:"static/old_style/setup.txt";s:4:"fb29";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"5323";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"240a";s:19:"share/locallang.xml";s:4:"e7a5";s:34:"marker/class.tx_ttboard_marker.php";s:4:"aba7";s:31:"view/class.tx_ttboard_forum.php";s:4:"ae37";s:32:"model/class.tx_ttboard_model.php";s:4:"d90f";}',
+	'version' => '1.2.3',
+	'_md5_values_when_last_written' => 'a:38:{s:9:"ChangeLog";s:4:"94ab";s:28:"class.tx_ttboard_wizicon.php";s:4:"3e44";s:16:"contributors.txt";s:4:"e2e4";s:21:"ext_conf_template.txt";s:4:"dbec";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"07a7";s:14:"ext_tables.php";s:4:"12a8";s:14:"ext_tables.sql";s:4:"4a67";s:23:"flexform_ds_pi_list.xml";s:4:"a333";s:23:"flexform_ds_pi_tree.xml";s:4:"2dde";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"b925";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"5939";s:14:"doc/manual.sxw";s:4:"0f0d";s:20:"lib/board_submit.inc";s:4:"e9e6";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"093f";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:23:"res/icons/fe/thread.gif";s:4:"9aac";s:31:"view/class.tx_ttboard_forum.php";s:4:"d7fa";s:36:"hooks/class.tx_ttboard_hooks_cms.php";s:4:"72c3";s:32:"model/class.tx_ttboard_model.php";s:4:"d90f";s:19:"share/locallang.xml";s:4:"e7a5";s:24:"template/board_help.tmpl";s:4:"ac9b";s:25:"template/board_notify.txt";s:4:"94a4";s:29:"template/board_template1.tmpl";s:4:"102b";s:29:"template/board_template2.tmpl";s:4:"6eee";s:29:"template/board_template3.tmpl";s:4:"7a11";s:34:"marker/class.tx_ttboard_marker.php";s:4:"aba7";s:30:"static/css_style/constants.txt";s:4:"67e5";s:26:"static/css_style/setup.txt";s:4:"3251";s:30:"static/old_style/constants.txt";s:4:"d955";s:26:"static/old_style/setup.txt";s:4:"fb77";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"7daf";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"93b4";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -45,6 +45,8 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 			'div2007' => '0.1.6-',
 		),
+	),
+	'suggests' => array(
 	),
 );
 
