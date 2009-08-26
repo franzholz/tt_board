4e285245b7c1bcfa53ce4e9829baab8c697d5973
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-26 20:48:55 +0000

    version 1.2.8 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_board/trunk@23691 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_board"
 #
-# Auto generated 25-09-2008 17:13
+# Auto generated 26-08-2009 22:47
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Simple threaded (tree) or list message board.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.2.7',
+	'version' => '1.2.8',
 	'dependencies' => 'cms,div2007',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:38:{s:9:"ChangeLog";s:4:"4192";s:28:"class.tx_ttboard_wizicon.php";s:4:"3e44";s:16:"contributors.txt";s:4:"e2e4";s:21:"ext_conf_template.txt";s:4:"aecc";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"07a7";s:14:"ext_tables.php";s:4:"12a8";s:14:"ext_tables.sql";s:4:"4a67";s:23:"flexform_ds_pi_list.xml";s:4:"a333";s:23:"flexform_ds_pi_tree.xml";s:4:"2dde";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"b925";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"5939";s:14:"doc/manual.sxw";s:4:"a472";s:20:"lib/board_submit.php";s:4:"863a";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"0c58";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:23:"res/icons/fe/thread.gif";s:4:"9aac";s:31:"view/class.tx_ttboard_forum.php";s:4:"7f49";s:36:"hooks/class.tx_ttboard_hooks_cms.php";s:4:"72c3";s:32:"model/class.tx_ttboard_model.php";s:4:"4bb7";s:19:"share/locallang.xml";s:4:"f288";s:24:"template/board_help.tmpl";s:4:"ac9b";s:25:"template/board_notify.txt";s:4:"a9ea";s:29:"template/board_template1.tmpl";s:4:"906a";s:29:"template/board_template2.tmpl";s:4:"4865";s:29:"template/board_template3.tmpl";s:4:"30f5";s:34:"marker/class.tx_ttboard_marker.php";s:4:"4e00";s:30:"static/css_style/constants.txt";s:4:"35f6";s:26:"static/css_style/setup.txt";s:4:"1a04";s:30:"static/old_style/constants.txt";s:4:"9b1b";s:26:"static/old_style/setup.txt";s:4:"83bf";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"0764";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"3171";}',
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"5439";s:28:"class.tx_ttboard_wizicon.php";s:4:"3e44";s:16:"contributors.txt";s:4:"e2e4";s:21:"ext_conf_template.txt";s:4:"aecc";s:12:"ext_icon.gif";s:4:"4881";s:15:"ext_icon__h.gif";s:4:"d4dd";s:17:"ext_localconf.php";s:4:"07a7";s:14:"ext_tables.php";s:4:"3474";s:14:"ext_tables.sql";s:4:"4a67";s:23:"flexform_ds_pi_list.xml";s:4:"a333";s:23:"flexform_ds_pi_tree.xml";s:4:"2dde";s:9:"forum.gif";s:4:"7c8f";s:13:"locallang.php";s:4:"255e";s:25:"locallang_csh_ttboard.php";s:4:"1a19";s:17:"locallang_tca.php";s:4:"b925";s:17:"message_board.gif";s:4:"d36b";s:7:"tca.php";s:4:"5939";s:14:"doc/manual.sxw";s:4:"b06d";s:20:"lib/board_submit.php";s:4:"2599";s:31:"lib/class.tx_ttboard_pibase.php";s:4:"96ed";s:28:"res/icons/fe/board_help1.gif";s:4:"1b80";s:23:"res/icons/fe/thread.gif";s:4:"9aac";s:31:"view/class.tx_ttboard_forum.php";s:4:"52d3";s:36:"hooks/class.tx_ttboard_hooks_cms.php";s:4:"72c3";s:32:"model/class.tx_ttboard_model.php";s:4:"a3c5";s:19:"share/locallang.xml";s:4:"f288";s:24:"template/board_help.tmpl";s:4:"ac9b";s:25:"template/board_notify.txt";s:4:"a9ea";s:29:"template/board_template1.tmpl";s:4:"2cf4";s:29:"template/board_template2.tmpl";s:4:"24a0";s:29:"template/board_template3.tmpl";s:4:"ac84";s:34:"marker/class.tx_ttboard_marker.php";s:4:"bbb5";s:20:"static/css/setup.txt";s:4:"161a";s:30:"static/css_style/constants.txt";s:4:"4303";s:26:"static/css_style/setup.txt";s:4:"55bb";s:30:"static/old_style/constants.txt";s:4:"b658";s:26:"static/old_style/setup.txt";s:4:"791e";s:36:"pi_list/class.tx_ttboard_pi_list.php";s:4:"0764";s:36:"pi_tree/class.tx_ttboard_pi_tree.php";s:4:"d963";}',
 	'suggests' => array(
 	),
 );
