af31d1560f19c379544267c0896ea31f66a14012
Franz Holzinger <holzinger@ttproducts.de>
2020-03-30 17:19:37 +0200

    div2007 in version 1.10.28 is needed

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'php' => '5.5.0-7.3.99',
             'typo3' => '8.7.0-9.5.99',
-            'div2007' => '1.10.15-0.0.0',
+            'div2007' => '1.10.28-0.0.0',
             'tslib_fetce' => '0.5.1-0.9.0',
         ),
         'conflicts' => array(
