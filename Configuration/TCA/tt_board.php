16118b0aeb55c9293cc57dc30c326afb8d9c0664
Franz Holzinger <holzinger@ttproducts.de>
2020-07-25 10:17:24 +0200

    introduce new PHP array notation with brackets

--- Configuration/TCA/tt_board.php
+++ Configuration/TCA/tt_board.php
@@ -6,78 +6,78 @@ $table = 'tt_board';
 // ******************************************************************
 // This is the standard Board table, tt_board
 // ******************************************************************
-$result = array(
-    'ctrl' => array (
+$result = [
+    'ctrl' => [
         'label' => 'subject',
-        'default_sortby' => 'ORDER BY parent,crdate DESC',		// crdate should gradually not be used! Trying to phase it out in favour of datetime.
+            'default_sortby' => 'ORDER BY parent,crdate DESC',		// crdate should gradually not be used! Trying to phase it out in favour of datetime.
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'delete' => 'deleted',
         'copyAfterDuplFields' => 'parent',
         'prependAtCopy' => 'LLL:EXT:lang/locallang_general.php:LGL.prependAtCopy',
-        'enablecolumns' => array (
+        'enablecolumns' => [
             'disabled' => 'hidden'
-        ),
+        ],
         'title' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:tt_board',
         'typeicon_column' => 'parent',
-        'typeicons' => array (
+        'typeicons' => [
             '0' => 'tt_faq_board_root.gif'
-        ),
+        ],
         'useColumnsForDefaultValues' => 'parent',
         'iconfile' => 'EXT:' . TT_BOARD_EXT . '/ext_icon.gif',
         'searchFields' => 'uid,author,email,subject,message,cr_ip',
-    ),
-    'interface' => array (
-        'showRecordFieldList' => 'subject,author,email,message'
-    ),
-    'columns' => array (
-        'subject' => array (
+    ],
+    'interface' => [
+        'showRecordFieldList' => 'subject,author,city,email,message'
+    ],
+    'columns' => [
+        'subject' => [
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.subject',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '40',
                 'max' => '256',
                 'eval' => 'null',
                 'default' => NULL,
-            )
-        ),
-        'message' => array (
+            ]
+        ],
+        'message' => [
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.message',
-            'config' => array (
+            'config' => [
                 'type' => 'text',
                 'cols' => '40',
                 'rows' => '5',
                 'eval' => 'null',
                 'default' => NULL,
-            )
-        ),
-        'author' => array (
+            ]
+        ],
+        'author' => [
             'label' => DIV2007_LANGUAGE_LGL . 'author',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '40',
                 'eval' => 'trim',
                 'max' => '80'
-            )
-        ),
-        'email' => array (
+            ]
+        ],
+        'email' => [
             'label'  => DIV2007_LANGUAGE_LGL . 'email',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '40',
                 'eval' => 'trim',
                 'max' => '80'
-            )
-        ),
-        'hidden' => array (
+            ]
+        ],
+        'hidden' => [
             'label'  => DIV2007_LANGUAGE_LGL . 'hidden',
-            'config' => array (
+            'config' => [
                 'type' => 'check'
-            )
-        ),
-        'parent' => array (
+            ]
+        ],
+        'parent' => [
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.parent',
-            'config' => array (
+            'config' => [
                 'type' => 'group',
                 'internal_type' => 'db',
                     'allowed' => $table,
@@ -85,49 +85,49 @@ $result = array(
                 'maxitems' => '1',
                 'minitems' => '0',
                 'show_thumbs' => '1'
-            )
-        ),
-        'notify_me' => array (
+            ]
+        ],
+        'notify_me' => [
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.notify_me',
-            'config' => array (
+            'config' => [
                 'type' => 'check'
-            )
-        ),
-        'crdate' => array (		// This field is by default filled with creation date. See tt_board 'ctrl' section
+            ]
+        ],
+        'crdate' => [		// This field is by default filled with creation date. See tt_board 'ctrl' section
             'exclude' => 1,
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.crdate',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '10',
                 'max' => '20',
                 'eval' => 'datetime'
-            )
-        ),
-        'cr_ip' => array (
+            ]
+        ],
+        'cr_ip' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.cr_ip',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '15',
                 'max' => '15',
-            )
-        ),
-        'reference' => array (
+            ]
+        ],
+        'reference' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:' . TT_BOARD_EXT . DIV2007_LANGUAGE_SUBPATH . 'locallang_tca.xlf:' . $table . '.reference',
-            'config' => array (
+            'config' => [
                 'type' => 'input',
                 'size' => '40',
                 'max' => '40',
                 'eval' => 'null',
                 'default' => null,
-            )
-        )
-    ),
-    'types' => array (
-        '0' => array('showitem' => 'hidden;;;;1-1-1, crdate, subject;;;;3-3-3, message, author, email, parent;;;;5-5-5, notify_me, cr_ip, reference')
-    )
-);
+            ]
+        ]
+    ],
+    'types' => [
+        '0' => ['showitem' => 'hidden;;;;1-1-1, crdate, subject;;;;3-3-3, message, author, email, parent;;;;5-5-5, notify_me, cr_ip, reference']
+    ]
+];
 
 return $result;
 
