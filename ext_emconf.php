0108981ac7794f672e06c29cbcb5e99c54a1fa97
Franz Holzinger <franz.holzinger@festwein.de>
2021-11-05 08:44:20 +0100

    PHP 7.2 is the minimum requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '8.7.0-9.5.99',
+            'php' => '7.2.0-7.4.99',
+            'typo3' => '9.5.0-10.4.99',
             'div2007' => '1.10.28-0.0.0',
             'tslib_fetce' => '0.5.1-0.9.0',
         ],
