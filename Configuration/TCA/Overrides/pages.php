350174b8674b20ab79de4a431d3c531dc38a95c3
Franz Holzinger <franz.holzinger@festwein.de>
2023-03-04 19:28:05 +0100

    bugfix new content element wizard

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,14 @@
+<?php
+
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') || die('Access denied.');
+
+call_user_func(function () {
+    ExtensionManagementUtility::registerPageTSConfigFile(
+        'tt_board',
+        'Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig',
+        'New Content Element Wizards'
+    );
+});
+
