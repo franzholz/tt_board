64f66f855b5ec8dd31960002ac3340107a56c502
Franz Holzinger <franz.holzinger@festwein.de>
2023-07-29 10:11:46 +0200

    feature 85160: Auto create management DB fields from TCA ctrl

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,13 +2,6 @@
 # Table structure for table 'tt_board'
 #
 CREATE TABLE tt_board (
-  uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-  pid int(11) unsigned DEFAULT '0' NOT NULL,
-  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  crdate int(11) unsigned DEFAULT '0' NOT NULL,
-  cruser_id int(11) DEFAULT '0' NOT NULL,
-  deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
-  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
   author varchar(80) DEFAULT '' NOT NULL,
   email varchar(80) DEFAULT '' NOT NULL,
   city varchar(255) DEFAULT '' NOT NULL,
