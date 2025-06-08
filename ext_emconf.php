ef18a59ae3fe8e13f390d765c40a5ee8a344c72a
Franz Holzinger <franz.holzinger@festwein.de>
2025-06-08 18:29:52 +0200

    TYPO3 13 is requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Message board, twin mode',
     'description' => 'Simple threaded (tree) or list message board (forum).',
     'category' => 'plugin',
-    'version' => '1.17.1',
+    'version' => '1.18.0',
     'state' => 'stable',
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
             'div2007' => '2.3.3-0.0.0',
             'tslib_fetce' => '0.9.1-0.15.0',
         ],
