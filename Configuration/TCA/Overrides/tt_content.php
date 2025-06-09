fb5e14b3a74c3874004e8d2f7bb86f087b80fd03
Franz Holzinger <franz.holzinger@festwein.de>
2025-06-09 13:56:52 +0200

    transform list into CType

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,33 +2,36 @@
 
 defined('TYPO3') || die('Access denied.');
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 call_user_func(function ($extensionKey, $table): void {
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['2'] = 'layout';
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist']['2'] = 'pi_flexform';
+    $pluginArray = ['2' => 'tree', '4' => 'list'];
+    foreach ($pluginArray as $k => $pluginType) {
+        $pluginSignature = $extensionKey . '_' . $pluginType;
 
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['4'] = 'layout';
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist']['4'] = 'pi_flexform';
+        ExtensionManagementUtility::addPlugin(
+            [
+                'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_tca.xlf:pi_' . $pluginType,
+                $pluginSignature,
+                'EXT:' . $extensionKey . '/Resources/Public/Icons/Extension.gif',
+                'plugin'
+            ],
+            'CType',
+            $extensionKey,
+        );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('2', 'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/flexform_ds_pi_tree.xml');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('4', 'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/flexform_ds_pi_list.xml');
+        // Activate the display of the FlexForm field
+        ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            '--div--;Configuration,pi_flexform,',
+            $pluginSignature,
+            'after:subheader',
+        );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-        [
-            'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_tca.xlf:pi_tree',
-            '2',
-            'EXT:' . $extensionKey . '/Resources/Public/Icons/Extension.gif'
-        ],
-        'list_type',
-        $extensionKey
-    );
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-        [
-            'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_tca.xlf:pi_list',
-            '4',
-            'EXT:' . $extensionKey . '/Resources/Public/Icons/Extension.gif'
-        ],
-        'list_type',
-        $extensionKey
-    );
+        ExtensionManagementUtility::addPiFlexFormValue(
+            '*',
+            'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/flexform_ds_pi_' . $pluginType . '.xml',
+            $pluginSignature,
+        );
+    }
 }, 'tt_board', basename(__FILE__, '.php'));
