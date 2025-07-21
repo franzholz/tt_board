03d0689ba2a7b70a6bd63288c0b21cd1acaf5e31
Franz Holzinger <franz.holzinger@festwein.de>
2025-07-21 16:15:48 +0200

    Icons.php

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,13 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'tt-board-tree' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'provider' => BitmapIconProvider::class,
         // The source bitmap file
         'source' => 'EXT:tt_board/Resources/Public/Icons/forum.gif'
     ],
     'tt-board-list' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'provider' => BitmapIconProvider::class,
         // The source bitmap file
         'source' => 'EXT:tt_board/Resources/Public/Icons/message_board.gif'
     ],
