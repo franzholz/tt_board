b8e3007dd23fc6340569f8b0173f5c90b5ed0be1
Franz Holzinger <franz.holzinger@festwein.de>
2026-06-15 18:00:54 +0200

    Update div2007 dependency version range

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-            'div2007' => '2.3.3-0.0.0',
+            'div2007' => '2.3.3-2.3.99',
             'tslib_fetce' => '0.9.5-0.15.0',
         ],
         'conflicts' => [
