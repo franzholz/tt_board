b4f41d504a7f89e3a0db86c3c5e3feeff8b739e0
Franz Holzinger <franz.holzinger@festwein.de>
2026-08-18 09:46:55 +0200

    remove ext_tables.php

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('TYPO3') || die('Access denied.');
-
-call_user_func(function ($extensionKey): void {
-    $table = 'tt_board';
-    $languageSubpath = '/Resources/Private/Language/';
-}, 'tt_board');
